6f431f28d666c532254f2a3154ade6761f36090f
Illia <illia.akhtyrtsev@smissltd.com>
2023-08-15 16:32:31 +0300

    basic config with db and static

--- artists/celery.py
+++ artists/celery.py
@@ -5,13 +5,13 @@ from statistics import mean
 from celery.schedules import crontab
 from django.conf import settings
 import logging
+import os
 
 
 logger = logging.getLogger(__name__)
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get("DJANGO_SETTINGS_MODULE"))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
-app = Celery('main')
+app = Celery('artists')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
--- artists/settings/local.py
+++ artists/settings/local.py
@@ -1,1 +1,17 @@
-import base
+from .base import *
+
+
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME") or "artists",
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+        "USER": os.environ.get("DB_USER") or "root",
+        "PASSWORD": os.environ.get("DB_PASS"),
+        "TEST": {"NAME": "artsists_test"},
+    },
+}
